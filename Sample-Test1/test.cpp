36730487ff0e5f2b9cf25564d73d3f336c2987bb
criterionim <criterion.im@gmail.com>
2024-04-11 10:13:24 +0900

    add gtest

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,14 @@
 #include "pch.h"
+#include "../Project14/calc.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, getGopTest) {
+	calc c;
+	EXPECT_EQ(c.getGop(1,2), 2);
+	EXPECT_EQ(c.getGop(3,6), 18);
+	EXPECT_EQ(c.getGop(4,5), 20);
 }
