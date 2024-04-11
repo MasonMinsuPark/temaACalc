bd48a241460454bb269eb87c9fc614f671e6965a
yeori977 <yeori977@gmail.com>
2024-04-11 10:10:23 +0900

    getSumSum 구현

--- Project14/calc.cpp
+++ Project14/calc.cpp
@@ -1,3 +1,6 @@
 class calc {
 public:
+	int getSumSum(int a, int b, int c) {
+		return a + b + c;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
+#include "../Project14/calc.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(calc, getSumSumTest) {
+	calc calculator;
+
+	EXPECT_EQ(calculator.getSumSum(0,0,0), 0);
+	EXPECT_EQ(calculator.getSumSum(1, 1, 1), 3);
+	EXPECT_EQ(calculator.getSumSum(-1, -1, -1), -3);
 }
